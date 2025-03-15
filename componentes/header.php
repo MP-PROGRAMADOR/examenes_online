71012da4d9e987e0ab8707ec1af05480b89e13bc
MP-PROGRAMADOR <mpprogramacion22@gmail.com>
2025-03-15 12:21:50 +0100

    cambios

--- componentes/header.php
+++ componentes/header.php
@@ -41,7 +41,7 @@
 
         <!--ESTILOS DE LA TABLA -->
   <!-- css datables nuevo -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
+
   <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap4.min.css">
  
   <script src="../assets/js/jquery.js"></script>
