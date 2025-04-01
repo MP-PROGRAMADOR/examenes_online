d8d7ba9afe428fea888759784fbe96b794765feb
MP-PROGRAMADOR <mpprogramacion22@gmail.com>
2025-04-01 12:01:49 +0100

    m

--- modulos/login/login.php
+++ modulos/login/login.php
@@ -1,7 +1,192 @@
- 
- <?php
-    session_start();
- ?>
+<?php
+
+
+require 'conexion/conexion.php';
+
+
+
+
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+
+    $usuario=mysqli_real_escape_string($conn, $_POST['usuario'] ) ;
+    
+    $password=mysqli_real_escape_string($conn, $_POST['password'] )  ;
+
+
+
+    if(!$usuario){
+        echo 'porfavor el usuario es necesario';
+    }
+
+
+
+    if(!$password){
+        echo 'porfavor el password es obligatorio';
+    }
+
+    if($usuario!=""){
+        $sql="SELECT `usuarios`.`nombre_usuario`, `usuarios`.`password_usuario`, `roles`.`nombre` FROM `usuarios` LEFT JOIN `roles` ON `usuarios`.`id_rol` = `roles`.`id_rol` where nombre_usuario='${usuario}'";
+        $resultado= mysqli_query($conn,$sql);
+
+       
+
+     
+
+      if($resultado->num_rows){
+
+
+        // verificar si el password es corecto
+
+        $usuario= mysqli_fetch_assoc($resultado);
+
+        $auth= password_verify($password, $usuario['password_usuario'] );
+
+        var_dump($auth);
+
+        echo 'usuario verificado';
+
+        if($auth){
+            // cuando existe el usuario y la contrasena
+
+        
+
+        $tipo_user=$usuario['nombre'];
+
+        // cuando el nombre de usuario existe
+
+        if($tipo_user=="DOCTOR"){
+
+            session_start();
+
+            $_SESSION['usuario']=$_POST['usuario'];
+           
+
+            header('Location: DOCTOR/index.php');
+
+        }
+
+
+        if($tipo_user=="ENFERMERA"){
+            
+            session_start();
+
+           $_SESSION['usuario']=$_POST['usuario'];
+
+            header('Location: ENFERMERA/index.php');
+
+        }
+        if($tipo_user=="LABORATORIO"){
+
+            session_start();
+
+            $_SESSION['usuario']=$_POST['usuario'];
+            
+            header('Location: LABORATORIO/pruebas.php');
+
+        } 
+        if($tipo_user=="ADMINISTRADOR"){
+
+            session_start();
+
+            $_SESSION['usuario']=$_POST['usuario'];
+            
+            header('Location: ADMINISTRADOR/index.php');
+
+        }
+        if($tipo_user=="PEDIATRIA"){
+
+            session_start();
+
+            $_SESSION['usuario']=$_POST['usuario'];
+            
+            header('Location: PEDIATRIA/doctor.php');
+
+        }
+
+
+
+
+        }else{
+
+            echo 'la contrasena es incorecta';
+        }
+
+      }else{
+        echo 'este usuario no existe';
+      }
+     
+        }
+
+    }
+
+    $hora_actual = date("H");
+
+
+
+    $hora = date("H:i:s");
+    $hora2 = 13;
+    $hora3 = 20;
+     
+    if($hora_actual < $hora2){
+        $saludo= "Buenos días";
+    }
+    else if($hora_actual > $hora2 AND $hora_actual < $hora3){
+        $saludo ="Buenas Tardes";
+    }
+    else{
+        $saludo= "Buenas Noches";
+    }
+
+
+    $fecha_actual="";
+    $vector = array(
+      1 => $fecha_actual . " Nada nuevo hay bajo el sol, pero cuántas cosas viejas hay que no conocemos.",
+      2 => $fecha_actual . " El verdadero amigo es aquel que está a tu lado cuando preferiría estar en otra parte.",
+      3 => $fecha_actual . " La sabiduría es la hija de la experiencia.",
+      4 => $fecha_actual . " Nunca hay viento favorable para el que no sabe hacia dónde va.",
+      6 => $fecha_actual . " El único modo de hacer un gran trabajo es amar lo que haces - Steve Jobs",
+      5 => $fecha_actual . " La felicidad es el verdadero sentimiento de plenitud que se consigue con el trabajo duro",
+      7 => $fecha_actual . " Sé un punto de referencia de calidad. Algunas personas no están acostumbradas a un ambiente donde la excelencia es aceptada",
+      8 => $fecha_actual . " La felicidad es el verdadero sentimiento de plenitud que se consigue con el trabajo duro",
+      9 => $fecha_actual . " Si no haces que ocurran  cosas, las cosas te ocurrirán a ti",
+      10 => $fecha_actual . " Trabajar en lo correcto es mucho más importante que trabajar duro",
+      11 => $fecha_actual . " Los líderes son encantadores, generan mucha empatía, se ponen en el lugar del resto para saber cómo piensa y que le deben decir, utilizan bastante su inteligencia emocional",
+      12 => $fecha_actual . " El trabajo obsesivo produce la locura, tanto como la pereza completa, pero con esta combinación se puede vivir",
+      13 => $fecha_actual . " En medio de la dificultad yace la oportunidad",
+      14 => $fecha_actual . " Los obstáculos son esas cosas espantosas que ves cuando quitas la mirada de tus metas",
+      15 => $fecha_actual . " El hombre que mueve montañas comienza cargando pequeñas piedras",
+      16 => $fecha_actual . " El fracaso no es lo opuesto al éxito: es parte del éxito",
+      17 => $fecha_actual . " La habilidad es lo que eres capaz de hacer. La motivación determina lo que haces. La actitud determina qué tan bien lo haces",
+      18 => $fecha_actual . " Somos lo que hacemos repetidamente. La excelencia, entonces, no es un acto, sino un hábito",
+      19 => $fecha_actual . " No tienes que mirar toda la escalera. Para empezar, solo concéntrate en dar el primer paso",
+      20 => $fecha_actual . " La felicidad no está en la mera posesión del dinero; radica en la alegría del logro, en la emoción del esfuerzo creativo",
+      21 => $fecha_actual . " Haz lo único que crees que no puedes hacer. Falla en eso. Intenta otra vez. Hazlo mejor la segunda vez. Las únicas personas que nunca se caen son aquellas que nunca se suben a la cuerda floja",
+      22 => $fecha_actual . " Nunca hay tiempo suficiente para hacerlo bien, pero siempre hay tiempo suficiente para hacerlo de nuevo",
+      23 => $fecha_actual . " Enfócate en ser productivo en vez de enfocarte en estar ocupado",
+      24 => $fecha_actual . " Trabajar en lo correcto es probablemente más importante que trabajar duro",
+      25 => $fecha_actual . " El hombre no puede descubrir nuevos océanos a menos que tenga el coraje de perder de vista la costa",
+      26 => $fecha_actual . " No aprendes a caminar siguiendo reglas. Aprendes haciendo y cayéndote",
+      27 => $fecha_actual . " Los obstáculos no tienen por qué detenerte. Si te topas con una pared, no te des la vuelta y te rindas. Descubre cómo escalarla, atravesarla o sortearla",
+      28 => $fecha_actual . " Nadie puede descubrirte hasta que tú lo hagas. Explota tus talentos, habilidades y fortalezas y haz que el mundo se siente y se dé cuenta",
+      29 => $fecha_actual . " Si hay algo que te asusta, entonces podría significar que vale la pena intentarlo",
+      30 => $fecha_actual . " El trabajo en equipo es el secreto que hace que gente común consiga resultados poco comunes",
+      );
+      $numero= rand(1,30);
+
+
+    
+?> 
+
+
+
+
+
+
+
+
+
+
 
 <!DOCTYPE html>
 <html lang="es">
@@ -246,14 +431,14 @@
                 <img src="imagen2.jpg" alt="Imagen de examen en pantalla">
                 <div class="carousel-caption">
                     <h3>Evalúa tu Progreso</h3>
-                    <p>Realiza exámenes online de forma fácil y segura.</p>
+                    <p><?php  echo $numero;  ?></p>
                 </div>
             </div>
             <div class="carousel-slide">
                 <img src="imagen3.jpg" alt="Imagen de estudiantes colaborando">
                 <div class="carousel-caption">
                     <h3>Conecta con tu Educación</h3>
-                    <p>Únete a nuestra plataforma y mejora tu rendimiento académico.</p>
+                    <p><?php  echo $numero;  ?></p>
                 </div>
             </div>
             <button class="carousel-prev">&#10094;</button>
