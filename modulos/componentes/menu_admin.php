5bca018c07a977a2f1d60168fca0b3dec180cd21
SIR-TOPOLÁ <sirtopola@gmail.com>
2025-04-07 15:57:48 +0100

    mejor camino nuevo

--- modulos/componentes/menu_admin.php
+++ modulos/componentes/menu_admin.php
@@ -1,35 +1,35 @@
 <div class="sidebar">
-        <div class="sidebar-logo">
-            Admin Panel
-        </div>
-        <ul class="sidebar-menu">
-            <li class="sidebar-menu-item">
-                <a href="#"><i class="fas fa-tachometer-alt"></i> Dashboard</a>
-            </li>
-            <li class="sidebar-menu-item">
-                <a href="../admin/categorias.php"><i class="fas fa-user-shield"></i> Gestión de categorias</a>
-            </li>
-            <li class="sidebar-menu-item">
-                <a href="../admin/escuelas.php"><i class="fas fa-user-shield"></i> Gestión de escuelas</a>
-            </li>
-            <li class="sidebar-menu-item">
-                <a href="../admin/examenes.php"><i class="fas fa-user-shield"></i> Gestión de examenes</a>
-            </li>
-            <li class="sidebar-menu-item">
-                <a href="../admin/preguntas.php"><i class="fas fa-user-shield"></i> Gestión de preguntas</a>
-            </li>
-            <li class="sidebar-menu-item">
-                <a href="../admin/estudiantes.php"><i class="fas fa-users"></i> Gestión de estudiantes</a>
-            </li>
-            <li class="sidebar-menu-item">
-                <a href="../admin/usuarios.php"><i class="fas fa-user"></i> Gestión de usuarios</a>
-            </li>
-            <!-- Añadir más ítems de menú según sea necesario -->
-            <li class="sidebar-menu-item">
-                <a href="ajustes.html"><i class="fas fa-cog"></i> Ajustes</a>
-            </li>
-            <li class="sidebar-menu-item">
-                <a href="../login/logout.php"><i class="fas fa-sign-out-alt"></i> Cerrar Sesión</a>
-            </li>
-        </ul>
+    <div class="sidebar-logo">
+        Admin Panel
     </div>
+    <ul class="sidebar-menu">
+        <li class="sidebar-menu-item">
+            <a href="#"><i class="bi bi-house-door"></i> Dashboard</a>
+        </li>
+        <li class="sidebar-menu-item">
+            <a href="../admin/categorias.php"><i class="bi bi-folder-lock"></i> Gestión de categorias</a>
+        </li>
+        <li class="sidebar-menu-item">
+            <a href="../admin/escuelas.php"><i class="bi bi-building"></i> Gestión de escuelas</a>
+        </li>
+        <li class="sidebar-menu-item">
+            <a href="../admin/examenes.php"><i class="bi bi-journal-text"></i> Gestión de examenes</a>
+        </li>
+        <li class="sidebar-menu-item">
+            <a href="../admin/preguntas.php"><i class="bi bi-question-circle"></i> Gestión de preguntas</a>
+        </li>
+        <li class="sidebar-menu-item">
+            <a href="../admin/estudiantes.php"><i class="bi bi-person-lines-fill"></i> Gestión de estudiantes</a>
+        </li>
+        <li class="sidebar-menu-item">
+            <a href="../admin/usuarios.php"><i class="bi bi-person-fill"></i> Gestión de usuarios</a>
+        </li>
+        <!-- Añadir más ítems de menú según sea necesario -->
+        <li class="sidebar-menu-item">
+            <a href="ajustes.html"><i class="bi bi-gear-fill"></i> Ajustes</a>
+        </li>
+        <li class="sidebar-menu-item">
+            <a href="../login/logout.php"><i class="bi bi-box-arrow-right"></i> Cerrar Sesión</a>
+        </li>
+    </ul>
+</div>
