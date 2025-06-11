6d48a129a97210125038965434fa84128ec355f2
unknown <salvadormete2@gmail.com>
2025-06-11 11:34:11 +0200

    hola

--- web_examenes (3).sql
+++ web_examenes (3).sql
@@ -11,6 +11,8 @@ drop database if exists web_examenes;
 create database web_examenes;
 use web_examenes;
 
+
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
@@ -180,11 +182,7 @@ CREATE TABLE `examenes` (
 --
 
 INSERT INTO `examenes` (`id`, `estudiante_id`, `categoria_id`, `asignado_por`, `fecha_asignacion`, `duracion`, `total_preguntas`, `estado`, `calificacion`, `codigo_acceso`) VALUES
-(5, 5, 4, 1, '2025-05-28 12:16:31', 7, 9, 'finalizado', 38.89, 'EXAM991276'),
-(6, 3, 4, 1, '2025-06-10 00:00:00', 4, 5, 'INICIO', NULL, 'EXAM707935'),
-(7, 4, 12, 1, '2025-06-12 00:00:00', 4, 5, 'INICIO', NULL, 'EXAM743015'),
-(9, 7, 6, 1, '2025-06-11 00:00:00', 4, 5, 'finalizado', 50.00, 'EXAM906468'),
-(10, 8, 6, 1, '2025-06-11 00:00:00', 4, 5, 'finalizado', 45.00, 'EXAM111646');
+(11, 5, 4, 1, '2025-06-15 00:00:00', 4, 5, 'INICIO', NULL, 'EXAM009514');
 
 -- --------------------------------------------------------
 
@@ -199,22 +197,6 @@ CREATE TABLE `examen_preguntas` (
   `respondida` tinyint(1) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Volcado de datos para la tabla `examen_preguntas`
---
-
-INSERT INTO `examen_preguntas` (`id`, `examen_id`, `pregunta_id`, `respondida`) VALUES
-(79, 9, 18, 1),
-(80, 9, 16, 1),
-(81, 9, 7, 1),
-(82, 9, 19, 1),
-(83, 9, 1, 1),
-(84, 10, 16, 1),
-(85, 10, 8, 1),
-(86, 10, 15, 1),
-(87, 10, 5, 1),
-(88, 10, 19, 1);
-
 -- --------------------------------------------------------
 
 --
@@ -228,13 +210,6 @@ CREATE TABLE `imagenes_pregunta` (
   `descripcion` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Volcado de datos para la tabla `imagenes_pregunta`
---
-
-INSERT INTO `imagenes_pregunta` (`id`, `pregunta_id`, `ruta_imagen`, `descripcion`) VALUES
-(5, 14, 'uploads/preguntas/6836ee991d6d2_Screenshot 2025-05-27 133121.png', NULL);
-
 -- --------------------------------------------------------
 
 --
@@ -247,47 +222,6 @@ CREATE TABLE `opciones_pregunta` (
   `texto` varchar(255) NOT NULL,
   `es_correcta` tinyint(1) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `opciones_pregunta`
---
-
-INSERT INTO `opciones_pregunta` (`id`, `pregunta_id`, `texto`, `es_correcta`) VALUES
-(1, 3, 'la misca es una pesca', 0),
-(2, 2, 'una copa se usa para almacenar agua', 0),
-(3, 1, 'se usa para aumentar la fuerza', 1),
-(4, 1, 'para eliminar el sueño', 1),
-(5, 1, 'no es un buen nutriente', 0),
-(6, 1, 'es de color chocolate', 1),
-(7, 5, '30 km/h', 0),
-(8, 5, '50 km/h', 1),
-(9, 5, '60 km/h', 0),
-(10, 5, '70 km/h', 0),
-(11, 6, 'Debe detenerse completamente al llegar a una señal de “Ceda el paso”.', 0),
-(12, 7, 'Cinturón de seguridad', 1),
-(13, 7, 'Radio del coche', 0),
-(14, 7, 'Luces durante la noche o en condiciones de baja visibilidad', 1),
-(15, 7, 'Espejos retrovisores en buen estado', 1),
-(16, 8, 'Puede avanzar con precaución', 0),
-(17, 8, 'Tiene prioridad sobre otros vehículos', 0),
-(18, 8, 'Debe detenerse completamente', 1),
-(19, 8, 'Puede continuar sin detenerse si no hay peatones', 0),
-(20, 9, 'Lluvia o pavimento mojado', 1),
-(21, 9, 'Neumáticos en mal estado', 1),
-(22, 9, 'Luz solar intensa', 0),
-(23, 9, 'Conducir a alta velocidad', 1),
-(28, 14, 'hola macho', 0),
-(29, 15, 'es un dispositivo de salida', 1),
-(30, 15, 'es un movil', 0),
-(31, 15, 'es un aparato domestico', 1),
-(32, 15, 'es un objeto que llora', 0),
-(33, 16, 'son los que beben leche', 0),
-(34, 16, 'son los que saben programar', 0),
-(35, 16, 'son los que arrasan dinero', 0),
-(36, 16, 'son los que saben organizarse', 1),
-(37, 17, 'un cubo es un recibiente cilindrico', 1),
-(38, 18, 'los moros son peces', 0),
-(39, 19, 'si respondes bien eres un buen chavo', 1);
 
 -- --------------------------------------------------------
 
@@ -304,26 +238,6 @@ CREATE TABLE `preguntas` (
   `creado_en` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Volcado de datos para la tabla `preguntas`
---
-
-INSERT INTO `preguntas` (`id`, `texto`, `tipo`, `tipo_contenido`, `activa`, `creado_en`) VALUES
-(1, '¿el café?', 'multiple', 'texto', 1, '2025-05-26 10:29:12'),
-(2, 'una copa se usa para almacenar agua', 'vf', 'texto', 1, '2025-05-26 10:23:18'),
-(3, 'la misca es una pesca', 'vf', 'texto', 1, '2025-05-21 08:59:27'),
-(5, '¿Cuál es la velocidad máxima permitida en zona urbana, salvo que se indique lo contrario?', 'multiple', 'texto', 1, '2025-05-28 11:03:12'),
-(6, 'Debe detenerse completamente al llegar a una señal de “Ceda el paso”.', 'vf', 'texto', 1, '2025-05-28 11:03:46'),
-(7, '¿Qué elementos de seguridad deben usarse obligatoriamente mientras se conduce?', 'multiple', 'texto', 1, '2025-05-28 11:04:58'),
-(8, '¿Qué indica una luz roja intermitente en un semáforo?', 'multiple', 'texto', 1, '2025-05-28 11:45:53'),
-(9, '¿Qué situaciones pueden aumentar la distancia de frenado de un vehículo?', 'multiple', 'texto', 1, '2025-05-28 11:47:19'),
-(14, 'hola macho', 'vf', 'ilustracion', 1, '2025-05-28 12:08:09'),
-(15, 'un altavos', 'multiple', 'texto', 1, '2025-06-10 14:03:46'),
-(16, 'los mejores programadores', 'unica', 'texto', 1, '2025-06-10 14:05:21'),
-(17, 'un cubo es un recibiente cilindrico', 'vf', 'texto', 1, '2025-06-10 14:06:16'),
-(18, 'los moros son peces', 'vf', 'texto', 1, '2025-06-10 14:06:34'),
-(19, 'si respondes bien eres un buen chavo', 'vf', 'texto', 1, '2025-06-10 14:06:57');
-
 -- --------------------------------------------------------
 
 --
@@ -335,52 +249,6 @@ CREATE TABLE `pregunta_categoria` (
   `pregunta_id` int(11) NOT NULL,
   `categoria_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `pregunta_categoria`
---
-
-INSERT INTO `pregunta_categoria` (`id`, `pregunta_id`, `categoria_id`) VALUES
-(2, 3, 4),
-(3, 2, 4),
-(4, 1, 4),
-(5, 7, 4),
-(6, 7, 6),
-(7, 5, 4),
-(8, 6, 4),
-(9, 8, 4),
-(10, 8, 5),
-(11, 8, 6),
-(12, 8, 7),
-(13, 8, 9),
-(14, 9, 1),
-(15, 9, 4),
-(16, 9, 6),
-(17, 1, 1),
-(18, 2, 1),
-(19, 3, 1),
-(20, 5, 1),
-(21, 6, 1),
-(22, 7, 1),
-(23, 8, 1),
-(26, 14, 1),
-(27, 14, 4),
-(28, 1, 6),
-(29, 5, 6),
-(30, 15, 4),
-(31, 15, 6),
-(32, 16, 1),
-(33, 16, 4),
-(34, 16, 6),
-(35, 17, 1),
-(36, 17, 4),
-(37, 17, 6),
-(38, 18, 1),
-(39, 18, 4),
-(40, 18, 6),
-(41, 19, 1),
-(42, 19, 4),
-(43, 19, 6);
 
 -- --------------------------------------------------------
 
@@ -394,28 +262,6 @@ CREATE TABLE `respuestas_estudiante` (
   `opcion_id` int(11) DEFAULT NULL,
   `fecha_respuesta` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `respuestas_estudiante`
---
-
-INSERT INTO `respuestas_estudiante` (`id`, `examen_pregunta_id`, `opcion_id`, `fecha_respuesta`) VALUES
-(286, 79, 18, '2025-06-10 15:33:18'),
-(287, 80, 34, '2025-06-10 15:33:49'),
-(288, 81, 12, '2025-06-10 15:34:14'),
-(289, 81, 14, '2025-06-10 15:34:14'),
-(290, 81, 15, '2025-06-10 15:34:14'),
-(291, 82, 19, '2025-06-10 15:34:21'),
-(292, 83, 3, '2025-06-10 15:34:46'),
-(293, 83, 4, '2025-06-10 15:34:46'),
-(294, 83, 6, '2025-06-10 15:34:46'),
-(295, 84, 34, '2025-06-10 15:54:59'),
-(296, 85, 16, '2025-06-10 15:55:23'),
-(297, 85, 19, '2025-06-10 15:55:23'),
-(298, 86, 29, '2025-06-10 15:55:33'),
-(299, 86, 31, '2025-06-10 15:55:33'),
-(300, 87, 10, '2025-06-10 15:55:54'),
-(301, 88, 19, '2025-06-10 15:56:03');
 
 -- --------------------------------------------------------
 
@@ -584,7 +430,7 @@ ALTER TABLE `estudiante_categorias`
 -- AUTO_INCREMENT de la tabla `examenes`
 --
 ALTER TABLE `examenes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT de la tabla `examen_preguntas`
