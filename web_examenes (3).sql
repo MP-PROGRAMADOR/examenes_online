51a416adbb8e9e4255c3373c731564254eaca8a9
unknown <salvadormete2@gmail.com>
2025-06-11 11:31:53 +0200

    hola

--- web_examenes (3).sql
+++ web_examenes (3).sql
@@ -7,6 +7,10 @@
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.0.30
 
+drop database if exists web_examenes;
+create database web_examenes;
+use web_examenes;
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
